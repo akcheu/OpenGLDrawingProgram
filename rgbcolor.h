13bde2194708dbf41a2d35b51bb03e19f600786b
akcheu <69490491+akcheu@users.noreply.github.com>
2021-03-16 02:50:52 -0700

    Add files via upload

---
+++ rgbcolor.h
@@ -0,0 +1,33 @@
+// Allen Cheung | OpenGL Project
+
+#ifndef __RGBCOLOR_H__
+#define __RGBCOLOR_H__
+
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+#include <GL/freeglut.h>
+
+struct rgbcolor {
+   union {
+      GLubyte ubvec[3];
+      struct {
+         GLubyte red {};
+         GLubyte green {};
+         GLubyte blue {};
+      } rgb {};
+   };
+   explicit rgbcolor() = default;
+   explicit rgbcolor (GLubyte red, GLubyte green, GLubyte blue):
+               rgb ({red, green,blue}) {};
+   explicit rgbcolor (const string&);
+   operator string() const;
+};
+
+ostream& operator<< (ostream&, const rgbcolor&);
+
+extern const std::unordered_map<std::string,rgbcolor> color_names;
+
+#endif
+
